b903b4c99b1a4a20eab7c460e23393315526d8e2
Abdelwakil RABIDI <abdelwakil.rabidi@beyn.io>
2023-03-15 17:30:53 +0100

    init project

---
+++ src/main/java/com/itexc/dom/DomApplication.java
@@ -0,0 +1,13 @@
+package com.itexc.dom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomApplication.class, args);
+	}
+
+}
