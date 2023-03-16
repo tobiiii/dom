0d3b62b3250c37f6b32c24293d86fb0b8d8c6c48
Abdelwakil RABIDI <abdelwakil.rabidi@beyn.io>
2023-03-16 17:54:25 +0100

    user repo

---
+++ src/main/java/com/itexc/dom/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.itexc.dom.repository;
+
+import com.itexc.dom.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+     Optional<User> findByEmailAddressIgnoreCase(String email);
+
+     boolean existsByEmailAddressIgnoreCase(String emailAddress);
+
+     Page<User> findAll(Pageable pageable);
+}
