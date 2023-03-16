7ccf6fce1bb8896e6e5d8fac6677f5bf05f1ed20
Abdelwakil RABIDI <abdelwakil.rabidi@beyn.io>
2023-03-16 17:59:18 +0100

    add SecurityAutoConfiguration

--- src/main/java/com/itexc/dom/DomApplication.java
+++ src/main/java/com/itexc/dom/DomApplication.java
@@ -2,8 +2,14 @@ package com.itexc.dom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+
+@ConfigurationPropertiesScan
+@EnableTransactionManagement
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class DomApplication {
 
 	public static void main(String[] args) {
